a49b10272b61e161dacc3bd0dbbf4ed88feef4f0
Lizz <lizwesterband@gmail.com>
2020-07-30 11:04:19 -0700

    [js, html and css]Add game incomplete; modal non functional

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # Breaking-Bricks
 This is my Project 1 For SEI 7/13
+I will be creating a game called Breaking Bricks using canvas.
+
+
+## Let's get started
+
+Our first step to creating our game is our HTML. We should start with a basic boilerplate, as well as a couple lines to define our canvas, and to connect our JavaScript and our CSS.
+
+```html
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Breaking Bricks Surprise</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <canvas id="canvas" width="900" height="750">
+        </canvas>  
+    </div>
+    <script src="app.js"></script>
+</body>
+</html>
+
+```
+
+I also included a div called container to put my canvas inside. Not absolutely necessary, but I have it for now in case I want to give any extra styling to my whole page.
+
+
+
+## What's next?
+
+Let's get our javascript set up. The first thing I will be including is a function that waits for the DOM to load before starting the game. After all, who wants to play a game that started before we got to it?
+
+```javascript
+document.addEventListener("DOMContentLoaded",
+  function () {
+
+  })
+
+```
+
+The above function should contain the rest of our javascript inside it.
+
+## Best Practices
+
+Whenever you are creating any project that involves code, you should follow some best practices. What are some of those?
+    
+    1. Declare your variables at the top of the page.
+    2. Make sure your code is formatted properly (indentation!!)
+    3. Drop Comments in your code, either to give yourself reminders, or to tell other devs what exactly is happening.
+    4. DRY code ( Don't repeat yourself)
+
+I also always throw a console.log at the very top of my JavaScript. That way I know right away whether or not my files are all connected.
---
+++ app.js
@@ -0,0 +1,202 @@
+console.log('here we go again');
+document.addEventListener('DOMContentLoaded', function() {
+	//declare your canvas and grab it from your HTML
+	let canvas = document.getElementById('canvas');
+	//get context; this game is two dimensional
+	let ctx = canvas.getContext('2d');
+	//declare ball size (can be changed to any size)
+	let ballSize = 20;
+	// the player stats
+	let score = 0;
+	let health = 6;
+	//declaring my left and right key as false,
+	// because they aren't in use until the game starts
+	let rightKey = false;
+	let leftKey = false;
+	//define x and y variables. this tells our ball where to show up on our page
+	//I want the ball to start in the middle, at the very bottom
+	let x = canvas.width / 2;
+	let y = canvas.height - 10;
+	//create our ball movement (can change the numbers to increase speed)
+	let xAdd = 4;
+	let ySub = -4;
+
+	//declaring our height and width of our board the ball bounces off of
+	let boardHeight = 20;
+	let boardWidth = 205;
+	let boardX = (canvas.width - boardWidth) / 2;
+	//all brick traits. 8 columns, 6 rows, height
+	// and width of each, padding on sides, and space from the top and left
+	let brickCol = 10;
+	let brickRow = 6;
+	let brickWidth = 75;
+	let brickHeight = 40;
+	let brickPad = 10;
+	let brickPushTop = 30;
+	let brickPushLeft = 30;
+	//event listeners for our left and right arrow keys. Will detect when pressed and released
+	document.addEventListener('keydown', keyPress, false);
+	document.addEventListener('keyup', keyRelease, false);
+
+	//following up with functions that reset our keys after they have been pressed or released
+	function keyPress(e) {
+		if (e.key == 'ArrowRight') {
+			rightKey = true;
+		} else if (e.key == 'ArrowLeft') {
+			leftKey = true;
+		}
+	}
+
+	function keyRelease(e) {
+		if (e.key == 'ArrowRight') {
+			rightKey = false;
+		} else if (e.key == 'ArrowLeft') {
+			leftKey = false;
+		}
+	}
+    //filling in our empty rows and columns with bricks
+	let bricks = [];
+	for (let c = 0; c < brickRow; c++) {
+		bricks[c] = [];
+		for (let r = 0; r < brickCol; r++) {
+			bricks[c][r] = { x: 0, y: 0, status: 1 };
+		}
+	}
+
+	//a function to detect a wall hit
+	//detects the x and y position of the ball are greater that the positions on the axis of the brick
+	//detects the x and y position of the ball is less than  the positions on the axis of the brick plus width and height
+	function wallHit() {
+		for (let c = 0; c < brickRow; c++) {
+			for (let r = 0; r < brickCol; r++) {
+				let b = bricks[c][r];
+				if (b.status == 1) {
+					if (x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
+						ySub = -ySub;
+						b.status = 0;
+						score++;
+						if (score == brickCol * brickRow) {
+							//our modal will show up here if the game is won
+							modal.innerHtml = 'You Won! Keanu is very proud.';
+							toggleClass(modal, 'show');
+							document.location.reload();
+						}
+					}
+				}
+			}
+		}
+	}
+
+	//
+	//this function creates the ball that will be used to break the bricks
+	function createBall() {
+		//our beginPath is where the creating of the ball starts.
+		//It will end this function at our closePath()
+		ctx.beginPath();
+		//the below line took a while. I used mdn
+		//and found this link, that explained using PI due tot he fact the ball is circular
+		//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/PI
+		ctx.arc(x, y, ballSize, 0, Math.PI * 2);
+		//pick our ball color
+		ctx.fillStyle = 'hotpink';
+		//color it in
+		ctx.fill();
+		ctx.closePath();
+	}
+	//this function does the same thing as the above,
+	//but it creates our board the ball bounces off of
+	function createBoard() {
+		ctx.beginPath();
+		ctx.rect(boardX, canvas.height - boardHeight, boardWidth, boardHeight);
+		ctx.fillStyle = 'aqua';
+		ctx.fill();
+		ctx.closePath();
+	}
+
+	//this function makes the bricks we get to destroy
+	function createBricks() {
+		for (let c = 0; c < brickRow; c++) {
+			for (let r = 0; r < brickCol; r++) {
+				//checking if our bricks have been hit or not. If hit, they are removed
+				if (bricks[c][r].status == 1) {
+					let brickX = r * (brickWidth + brickPad) + brickPushLeft;
+					let brickY = c * (brickHeight + brickPad) + brickPushTop;
+					bricks[c][r].x = brickX;
+					bricks[c][r].y = brickY;
+					ctx.beginPath();
+					ctx.rect(brickX, brickY, brickWidth, brickHeight);
+					ctx.fillStyle = 'yellowgreen';
+					ctx.fill();
+					ctx.closePath();
+				}
+			}
+		}
+	}
+	//keeping track of our score (amount of bricks broken) and our health
+	function scoreTracker() {
+		ctx.font = '20px Helvetica';
+		ctx.fillStyle = 'red';
+		ctx.fillText('Score: ' + score, 8, 20);
+	}
+	function healthTracker() {
+		ctx.font = '20px Helvetica';
+		ctx.fillStyle = 'red';
+		ctx.fillText('Health: ' + health, canvas.width - 85, 20);
+	}
+	//this function pulls in the rest of the functions and creates them
+	//our last big funtion to pull the game together
+	function create() {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		createBricks();
+		createBall();
+		createBoard();
+		scoreTracker();
+		healthTracker();
+		wallHit();
+
+		//if statements to keep our ball moving( or to continue to create the illusion it is moving)
+		if (x + xAdd > canvas.width - ballSize || x + xAdd < ballSize) {
+			xAdd = -xAdd;
+		}   //nested if statement, speaking on the y axis instead of the x axis above
+            if (y + ySub < ballSize) {
+                ySub = -ySub;
+            } else if (y + ySub > canvas.height - ballSize) {
+                if (x > boardX && x < boardX + boardWidth) {
+                    ySub = -ySub;
+                } else {
+                    // else statement saying our health goes down by one everytime the ball misses the board
+                    health--;
+                    //if statement saying if our helath runs out, the game is over
+                    if (!health) {
+                        //why doesnt my modal work
+                        modal.innerHTML = "Game Over. Keanu wouldn't want you to give up. Play again?";
+                        toggleClass(modal, 'show');
+                        document.location.reload();
+                        clearInterval(interval);
+				} else {
+					//if we still have health, continue to move out ball on the screen
+					x = canvas.width / 2;
+					y = canvas.height - 10;
+					xAdd = 5;
+					ySub = -5;
+					boardX = (canvas.width - boardWidth) / 2;
+				}
+			}
+		}
+		//these if else statements tell our board how many
+		//pixels to move when our arrow keys are pressed
+		//so long as the board will still be *inside* the canvas
+		if (rightKey && boardX < canvas.width - boardWidth) {
+			boardX += 9;
+		} else if (leftKey && boardX > 0) {
+			boardX -= 9;
+		}
+		//redefining x and y to keep our ball moving
+		x += xAdd;
+        y += ySub;
+        //informing our browser we would like to animate our game and create all our elements
+		requestAnimationFrame(create);
+	}
+
+	create();
+});
